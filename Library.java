358b6aff61e6a5633333fb6d8c20598d6ea00b22
pavani2004 <115790735+pavani2004@users.noreply.github.com>
2023-03-26 16:07:59 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+
+public class Library {
+
+		public static void main(String[] args)
+	{
+	
+		Scanner sc = new Scanner(System.in);
+
+		
+        System.out.println("");
+		System.out.println(
+			"Welcome to the digtal Library!");
+		System.out.println(
+			"	       Select From The Following Options:			 ");
+		System.out.println(" ");
+
+		
+		Books ob = new Books();
+		
+		Students obStudent = new Students();
+
+		int choice;
+		int searchChoice;
+		String books;
+		String Authors;
+
+		
+		do {
+
+			ob.dispMenu();
+			choice = sc.nextInt();
+
+			
+			switch (choice) {
+
+				
+			case 1:
+				Book b = new Book();
+				ob.addBook(b);
+				break;
+
+				
+			case 2:
+				ob.upgradeBQuantity();
+				break;
+
+			
+			case 3:
+
+				System.out.println(
+					" press 1 to Search with Book Serial No.");
+				System.out.println(
+					" Press 2 to Search with Book's Author Name.");
+				searchChoice = sc.nextInt();
+
+				
+				switch (searchChoice) {
+
+					
+				case 1:
+					ob.searchBySNo();
+					break;
+
+					
+				case 2:
+						ob.searchByAuthorName();
+				break;
+
+				}
+			case 4:
+				ob.showAllBooks();
+				break;
+
+			
+			case 5:
+				Student s = new Student();
+				obStudent.addStudent(s);
+				break;
+
+				
+			case 6:
+				obStudent.showAllStudents();
+				break;
+
+				
+			case 7:
+				obStudent.checkOutBook(ob);
+				break;
+
+				
+			case 8:
+				obStudent.checkInBook(ob);
+				break;
+
+			default:
+				System.out.println("ENTER BETWEEN 0 TO 8.");
+			}
+
+		}
+		while (choice != 0);
+	}
+}
